0c68b7c0fe277a587fdc6088cbbd405f63e08ab6
Ilya Gomel <ilya.gomel@dataart.com>
2019-07-22 15:27:56 +0300

    added front-end

--- server/index.js
+++ server/index.js
@@ -7,15 +7,31 @@ let server = http.createServer(app);
 let socketIO = require('socket.io');
 let io = socketIO(server);
 
-const port = process.env.PORT || 3000;
+let axios = require('axios');
 
-io.on('connection', (socket)=>{
-    console.log('user connected');
-    socket.on('new-message', (message) => {
-        io.emit('new-message', message);
-    });
-});
+const port = process.env.PORT || 3001;
+
+let interval;
+
+io.on("connection", socket => {
+    console.log("New client connected"), setInterval(
+      () => getApiAndEmit(socket),
+      1000
+    );
+    socket.on("disconnect", () => console.log("Client disconnected"));
+  });
 
 server.listen(port, ()=>{
    console.log(`listening on port: ${port}`);
 });
+
+const getApiAndEmit = async socket => {
+    try {
+      const res = await axios.get(
+        "https://api.darksky.net/forecast/4207eea0f0009097154e6ad153557622/43.7695,11.2558"
+      ); // Getting the data from DarkSky
+      socket.emit("FromAPI", res.data.currently.temperature); // Emitting a new message. It will be consumed by the client
+    } catch (error) {
+      console.error(`Error: ${error.code}`);
+    }
+  };
